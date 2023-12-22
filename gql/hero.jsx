3ed6e8ca7e5adc10e7fe8e40c0ef49b60f5bfa8f
Amerey <59344168+Amereyeu@users.noreply.github.com>
2023-12-22 15:21:08 +0100

    Update hero.jsx

--- gql/hero.jsx
+++ gql/hero.jsx
@@ -1,1 +1,13 @@
 
+function Hero({ title, option }) {
+  return (
+    <div className="hero-wrap">
+      <div className={`hero hero--${option}`}>
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
+
