cee781f2eb942d9cb11817375c9a6158b3804227
Amerey <59344168+Amereyeu@users.noreply.github.com>
2023-12-27 18:16:33 +0100

    Update footer.jsx

--- gql/footer.jsx
+++ gql/footer.jsx
@@ -1,1 +1,42 @@
 
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+import data from "../../data/nav.json";
+
+function Footer() {
+  const today = new Date();
+  const [navigationData] = useState(data);
+
+  return (
+    <div className="footer-wrap">
+      <footer className="footer">
+        <div className="footer__logo">
+          <Link to="/">
+            <h1>P</h1>
+          </Link>
+        </div>
+        <div className="footer__links">
+          <ul>
+            {navigationData.map((navItem, i) => (
+              <li key={i}>
+                <HashLink
+                  smooth
+                  to={navItem.to}
+                  className="footer__links__item">
+                  {navItem.name}
+                </HashLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </footer>
+
+      <p className="copy">&copy; 2008 - {today.getFullYear()} P</p>
+    </div>
+  );
+}
+
+export default Footer;
+
