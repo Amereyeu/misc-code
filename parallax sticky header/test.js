c8a5f0c61afe7c897d4949c9cae08c0323c97a16
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-02-05 22:17:22 +0100

    Update test.js

--- parallax sticky header/test.js
+++ parallax sticky header/test.js
@@ -1,1 +1,29 @@
 
+
+var initialPos =  $('#logo').offset().top;
+
+$(window).scroll(function() {
+  
+    var scrolled = $(window).scrollTop();
+  
+    if (scrolled > initialPos) {
+      
+      $('#logo').css({
+          position:"fixed",
+          top:'0px',
+          transform: "scale(0.8)" 
+       });
+      
+    } else {
+
+      var scale = 1 - (scrolled/initialPos);      
+      scale = (scale * 1.2) + 0.8;
+      
+      $('#logo').css({
+            position:"absolute",
+            top:initialPos+"px",
+            transform: "scale("+scale+") translate(0%, -"+scrolled/(scale*scrolled)+"%)"
+       });
+    }
+    
+});
