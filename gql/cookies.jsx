75ab799d742af419717856cc62357f334d35b1cf
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-02-18 20:09:28 +0100

    Update cookies.jsx

--- gql/cookies.jsx
+++ gql/cookies.jsx
@@ -1,1 +1,28 @@
 
+import { useCookies } from "react-cookie";
+
+function Cookies() {
+  const [cookies, setCookie] = useCookies(["cookieConsent"]);
+
+  var cookieExpire = new Date();
+  cookieExpire.setDate(cookieExpire.getDate() + 7);
+
+  const giveCookieConsent = () => {
+    // setCookie("cookieConsent", true, { path: "/" }); //session cookie
+    setCookie("cookieConsent", true, { expires: cookieExpire }); //expires in 7 days
+  };
+
+  return (
+    <div id="gdpr">
+      <p>
+        Tento web používá soubory cookie. Dalším procházením tohoto webu
+        vyjadřujete souhlas s jejich používáním.
+      </p>
+      <button id="cookieButton" onClick={giveCookieConsent}>
+        Rozumím
+      </button>
+    </div>
+  );
+}
+
+export default Cookies;
