5ac392be6d83c845a325714f3caee75a5122c573
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-03-06 22:18:11 +0100

    Update pwstrengthmeter.jsx

--- gql/pwstrengthmeter.jsx
+++ gql/pwstrengthmeter.jsx
@@ -1,1 +1,36 @@
 
+import { useState } from "react";
+
+import PasswordStrengthBar from "react-password-strength-bar";
+
+export const RegisterForm = () => {
+
+  const [password, setPassword] = useState("");
+
+
+
+  return (
+    <form>
+
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.currentTarget.value)}
+      />
+      <PasswordStrengthBar
+        password={password}
+        scoreWords={[
+          "bad",
+          "okay",
+          "good",
+          "strong",
+          "perfect!",
+        ]}
+        shortScoreWord={"bad"}
+      />
+
+    </form>
+  );
+};
+
+export default RegisterForm;
