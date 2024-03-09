8164dba02a034be08f98ab0f69c317c544006f48
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-03-09 16:47:57 +0100

    Update schema.jsx

--- gql/schema.jsx
+++ gql/schema.jsx
@@ -1,1 +1,19 @@
+import { object, string } from "yup";
+
+const requiredField = "Toto pole je třeba vyplnit";
+
+const emailPattern = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i;
+
+export const contactFormSchema = object({
+  name: string()
+    .min(3, "Minimální počet znaků je 3.")
+    .max(20, "Maximální počet znaků je 20.")
+    .required(requiredField),
+  email: string()
+    .matches(emailPattern, {
+      message: " Vložte emailovou adresu ve správném formátu.",
+    })
+    .required(requiredField),
+  message: string().required(requiredField),
+});
 
