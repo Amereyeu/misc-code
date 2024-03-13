e4bf76fb0f8625e161edd7702de8069bd4ee5722
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-03-13 18:13:41 +0100

    Update login.jsx

--- gql/login.jsx
+++ gql/login.jsx
@@ -1,1 +1,62 @@
 
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isIconClicked, setIsIconClicked] = useState(false);
+
+  const handlePassword = () => {
+    setIsIconClicked((prevState) => !prevState);
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    login(username, password);
+    console.log("username:", username);
+    console.log("password", password);
+  };
+
+  return (
+    <div className="form-wrap">
+      <form id="contact-form" role="form" onSubmit={handleLogin}>
+        <div className="form-row">
+          <div className="form-group">
+            <input
+              id="name"
+              type="text"
+              placeholder="Name"
+              className={`form-control user `}
+            />
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <input
+              id="password"
+              type={`${isIconClicked ? "text" : "password"}`}
+              placeholder="Password"
+              className="form-control"
+            />
+            <div
+              className={`icon ${isIconClicked ? "eye-open" : "eye-closed"}`}
+              onClick={handlePassword}
+            ></div>
+          </div>
+        </div>
+
+        <div className="form-row">
+          <div className="form-group">
+            <button className="send" type="submit">
+              Login
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
