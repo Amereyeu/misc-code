df1ce2e14ca27197f62a6fc3ced48b021b75b40f
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-05-03 17:46:11 +0200

    Update test.js

--- js flex image gallery/test.js
+++ js flex image gallery/test.js
@@ -1,1 +1,18 @@
 
+const slides = document.querySelectorAll(".slide");
+
+function toggleOpen() {
+  this.classList.toggle("open");
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active");
+  }
+}
+
+slides.forEach((slide) => slide.addEventListener("click", toggleOpen));
+
+slides.forEach((slide) =>
+  slide.addEventListener("transitionend", toggleActive)
+);
