6a5a50b8b49631ea576f30cde48b18cdca75bf8c
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-05-12 14:39:35 +0200

    Update test.js

--- typing text js/test.js
+++ typing text js/test.js
@@ -1,1 +1,47 @@
+async function init() {
+  const node = document.querySelector("#type-text");
+
+  await sleep(1000);
+  node.innerText = "";
+  await node.type("Good, ");
+
+  while (true) {
+    await node.type("Morning!");
+    await sleep(2000);
+    await node.delete("Morning!");
+    await node.type("Afternoon!");
+    await sleep(2000);
+    await node.delete("Afternoon!");
+    await node.type("Evening!");
+    await sleep(2000);
+    await node.delete("Evening!");
+  }
+}
+
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+class TypeAsync extends HTMLSpanElement {
+  get typeInterval() {
+    const randomMs = 100 * Math.random();
+    return randomMs < 50 ? 10 : randomMs;
+  }
+
+  async type(text) {
+    for (let character of text) {
+      this.innerText += character;
+      await sleep(this.typeInterval);
+    }
+  }
+
+  async delete(text) {
+    for (let character of text) {
+      this.innerText = this.innerText.slice(0, this.innerText.length - 1);
+      await sleep(this.typeInterval);
+    }
+  }
+}
+
+customElements.define("type-async", TypeAsync, { extends: "span" });
+
+init();
 
