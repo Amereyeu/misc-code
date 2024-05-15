c37cf01aa019d5037468884ac1beb53b6a5a9fe9
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-05-15 16:10:11 +0200

    Update test.js

--- popup window js/test.js
+++ popup window js/test.js
@@ -1,1 +1,8 @@
 
+const toggle = document.querySelectorAll(".toggle");
+
+function toggleNewsLetter() {
+  newsLetter.classList.toggle("active");
+}
+
+toggle.forEach((t) => t.addEventListener("click", toggleNewsLetter));
