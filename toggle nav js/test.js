fb2409eae73533d65d11bab26f10b3e4f7108210
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-06-22 14:34:47 +0200

    Update test.js

--- toggle nav js/test.js
+++ toggle nav js/test.js
@@ -1,1 +1,24 @@
 
+const toggleNavSection = () => {
+  let lastScrollTop = 0;
+  const navbar = document.querySelector(".navbar");
+
+  const toggleNavOnScroll = () => {
+    const scroll = document.documentElement.scrollTop;
+
+    if (scroll > lastScrollTop && scroll > 100) {
+      navbar.classList.add("hide");
+    } else if (scroll < lastScrollTop) {
+      navbar.classList.remove("hide");
+    }
+
+    lastScrollTop = scroll <= 0 ? 0 : scroll;
+  };
+
+  if (navbar) {
+    window.removeEventListener("scroll", toggleNavOnScroll);
+    window.addEventListener("scroll", toggleNavOnScroll);
+  }
+};
+
+toggleNavSection();
