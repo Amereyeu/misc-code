e1e130c836180319910745958a2b534da0299b78
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-10 07:25:15 +0200

    Update test.js

--- snap scroll carouselle/test.js
+++ snap scroll carouselle/test.js
@@ -1,1 +1,14 @@
 
+let list = document.querySelector(".list");
+let item = document.querySelector(".list__item");
+let itemWidth = item.offsetWidth;
+
+document
+  .querySelector(".button.previous")
+  .addEventListener("click", function () {
+    list.scrollBy({ left: -itemWidth, behavior: "smooth" });
+  });
+
+document.querySelector(".button.next").addEventListener("click", function () {
+  list.scrollBy({ left: itemWidth, behavior: "smooth" });
+});
