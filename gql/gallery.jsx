cd76ebb385065ffc2b7d31052ab5a90639745059
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-13 22:39:59 +0200

    Update gallery.jsx

--- gql/gallery.jsx
+++ gql/gallery.jsx
@@ -1,1 +1,25 @@
 
+/*********************************************
+ * gallery section
+ *********************************************/
+
+import { useState } from "react";
+import galleryItems from "../../data/gallery";
+
+function Gallery() {
+  const [data] = useState(galleryItems);
+
+  return (
+    <section className="gallery-wrap" id="gallery">
+      <h2>Galerie</h2>
+
+      <div className="gallery">
+        {data.map((image, i) => (
+          <img src={image} alt={i} className="gallery__image" key={i} />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Gallery;
