d2b9f7ed5612f7bbd53381d3ffecaa9ac9d09fc8
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-26 12:44:04 +0200

    Update price.js

--- react/tabs/price.js
+++ react/tabs/price.js
@@ -1,1 +1,38 @@
 
+/*********************************************
+ * price list
+ *********************************************/
+
+const priceItems = [
+  {
+    name: "tab1",
+    label: "Oil change",
+    items: [
+      { text: "Nunc dignissim risus id metus", price: "300" },
+      { text: "Ut aliquam sollicitudin leo", price: "1300" },
+      { text: "Cras ornare tristique elit", price: "30" },
+      { text: "Cras ornare tristique elit", price: "5530" },
+      { text: "Nunc dignissim risus id metus", price: "3000" },
+      { text: "Ut aliquam sollicitudin leo", price: "1300" },
+    ],
+  },
+  {
+    name: "tab2",
+    label: "Engine",
+    items: [
+      { text: "Ut aliquam sollicitudin leo", price: "130" },
+      { text: "Cras ornare tristique elit", price: "30" },
+    ],
+  },
+  {
+    name: "tab3",
+    label: "Transmission",
+    items: [
+      { text: "Cras ornare tristique elit", price: "5530" },
+      { text: "Nunc dignissim risus id metus", price: "3000" },
+      { text: "Ut aliquam sollicitudin leo", price: "1300" },
+    ],
+  },
+];
+
+export default priceItems;
