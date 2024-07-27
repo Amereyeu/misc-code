5ba8cc51c6077650fac653804d9c67ae94f50b4f
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-27 10:09:01 +0200

    Update gallery.js

--- react/modal image gallery/gallery.js
+++ react/modal image gallery/gallery.js
@@ -1,1 +1,15 @@
 
+/*********************************************
+ * images for gallery
+ *********************************************/
+
+const galleryItems = [
+  "images/engine.jpg",
+  "images/frame.jpg",
+  "images/sedan.jpg",
+  "images/merc.jpg",
+  "images/work.jpg",
+  "images/tools.jpg",
+];
+
+export default galleryItems;
