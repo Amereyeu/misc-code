6e69f04a4f50d4134b2071762c2076a6f21cb52a
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-28 12:59:23 +0200

    Update cta.jsx

--- react/cta/cta.jsx
+++ react/cta/cta.jsx
@@ -1,1 +1,66 @@
 
+/*********************************************
+ * our services section
+ *********************************************/
+import { motion } from "framer-motion";
+import { HashLink } from "react-router-hash-link";
+
+import { Phone } from "./phone";
+
+function Cta() {
+  return (
+    <section className="cta">
+      <motion.div
+        className="cta__item"
+        initial={{
+          opacity: 0,
+          x: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+          transition: {
+            duration: 2.5,
+          },
+        }}
+        viewport={{ once: true }}
+      >
+        <h2>Call now</h2>
+
+        <div className="cta__item__block">
+          <Phone />
+          <h3>+420 123 456 789</h3>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="cta__item"
+        initial={{
+          opacity: 0,
+          x: 0,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+          transition: {
+            duration: 2.5,
+          },
+        }}
+        viewport={{ once: true }}
+      >
+        <h2>Appointment</h2>
+
+        <HashLink
+          smooth
+          to="/kontakt#form"
+          className="cta__item__button"
+          duration={50}
+        >
+          Make an appointment
+        </HashLink>
+      </motion.div>
+    </section>
+  );
+}
+
+export default Cta;
