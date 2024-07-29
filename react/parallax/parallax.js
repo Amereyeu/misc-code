74a8639f163cdad6b1bd529ea5c318856393f35c
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-29 09:27:52 +0200

    Update parallax.js

--- react/parallax/parallax.js
+++ react/parallax/parallax.js
@@ -1,1 +1,39 @@
 
+/*********************************************
+ * parallax items
+ *********************************************/
+
+const parallaxItems = [
+  {
+    icon: "images/gear.svg",
+    title: "20 let na trhu",
+    text: "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna ",
+  },
+  {
+    icon: "images/gear.svg",
+    title: "Profesionální lektoři",
+    text: "Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada.",
+  },
+  {
+    icon: "images/gear.svg",
+    title: "Projekty EU",
+    text: "Ut convallis, sem sit amet interdum consectetuer, odio augue aliquam leo, nec dapibus tortor nibh sed augue. Integer eu magna sit amet metus fermentum posuere. Morbi sit amet nulla sed dolor elementum imperdiet. Quisque fermentum. Cum sociis natoque ",
+  },
+  {
+    icon: "images/gear.svg",
+    title: "50 ukončených projektů",
+    text: "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue.",
+  },
+  {
+    icon: "images/gear.svg",
+    title: "2200 spokojených studentů",
+    text: "Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. Sed vel lacus. Mauris nibh felis, adipiscing varius, adipiscing in, lacinia vel, tellus. Suspendisse.",
+  },
+  {
+    icon: "images/gear.svg",
+    title: "Vzdělávací a rekfalifikační kurzy",
+    text: "Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper Nunc iaculis mi in ante. Vivamus imperdiet nibh feugiat est.",
+  },
+];
+
+export default parallaxItems;
