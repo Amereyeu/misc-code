9d6ea984f49599479856f2e61d79cf9d88dfa602
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-07-31 16:13:56 +0200

    Update review.js

--- react/reviews/review.js
+++ react/reviews/review.js
@@ -1,1 +1,21 @@
 
+/*********************************************
+ * review items
+ *********************************************/
+
+const sliderItems = [
+  {
+    author: "John Walker",
+    text: "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus. Praesent elementum hendrerit tortor. ",
+  },
+  {
+    author: "Winnie Naleher",
+    text: "Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus. ",
+  },
+  {
+    author: "Jackie O.",
+    text: "Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada. ",
+  },
+];
+
+export default sliderItems;
