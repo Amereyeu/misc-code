7c27a4c3c0d3e6bc9cf9ff20407bcc82fce617dd
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-08-07 14:28:39 +0200

    Update test.js

--- random bg/test.js
+++ random bg/test.js
@@ -1,1 +1,6 @@
 
+document.getElementById("btn").addEventListener("click", function () {
+  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+  document.documentElement.style.setProperty("--random-color", randomColor);
+});
