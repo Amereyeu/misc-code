a5fee092dae2c2a291739371e9e529fdefdff45e
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-08-10 19:25:02 +0200

    Update typing.jsx

--- react/typing text/typing.jsx
+++ react/typing text/typing.jsx
@@ -1,1 +1,26 @@
 
+import { TypeAnimation } from "react-type-animation";
+
+function Typing() {
+  return (
+    <TypeAnimation
+      sequence={[
+        "Good Morning",
+        1000,
+        "Good Afternoom",
+        1000,
+        "Good Evening",
+        1000,
+      ]}
+      wrapper="div"
+      className="typewriter"
+      speed={50}
+      cursor={true}
+      repeat={Infinity}
+    />
+
+    // https://react-type-animation.netlify.app/examples
+  );
+}
+
+export default Typing;
