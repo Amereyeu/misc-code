9068214e12e541a61025e8b1dca09e55ea084e2c
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-08-21 10:45:47 +0200

    Update questions.js

--- react/faq/questions.js
+++ react/faq/questions.js
@@ -1,1 +1,29 @@
 
+/*********************************************
+ * questions and answers
+ *********************************************/
+
+const questionsItems = [
+  {
+    question: "Question 1",
+    answer:
+      "Morbi interdum mollis sapien. Sed ac risus. Phasellus lacinia, magna a ullamcorper laoreet, lectus arcu pulvinar risus, vitae facilisis libero dolor a purus.",
+  },
+  {
+    question: "Question 2",
+    answer:
+      "Ut convallis, sem sit amet interdum consectetuer, odio augue aliquam leo, nec dapibus tortor nibh sed augue. Integer eu magna sit amet metus fermentum posuere. Morbi sit amet nulla sed dolor elementum imperdiet. Quisque fermentum.",
+  },
+  {
+    question: "Question 3",
+    answer:
+      "Suspendisse mauris. Fusce accumsan mollis eros. Pellentesque a diam sit amet mi ullamcorper vehicula. Integer adipiscing risus a sem. Nullam quis massa sit amet nibh viverra malesuada. Nunc sem lacus, accumsan quis, faucibus non, congue vel, arcu. Ut scelerisque hendrerit tellus. Integer sagittis. Vivamus a mauris eget arcu gravida tristique. Nunc iaculis mi in ante. Vivamus imperdiet nibh feugiat est.",
+  },
+  {
+    question: "Question 4",
+    answer:
+      "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus.",
+  },
+];
+
+export default questionsItems;
