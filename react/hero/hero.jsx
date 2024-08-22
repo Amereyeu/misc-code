27200caca69bd060bdae520c7ff558d80e6c03fa
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-08-22 19:41:11 +0200

    Update hero.jsx

--- react/hero/hero.jsx
+++ react/hero/hero.jsx
@@ -1,1 +1,16 @@
+/*********************************************
+ * hero section in courses
+ *********************************************/
+
+function Hero({ title, option }) {
+  return (
+    <div className="hero-wrap">
+      <div className={`hero hero--${option}`}>
+        <h2>{title}</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
 
