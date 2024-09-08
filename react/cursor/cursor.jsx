53b9228af86d8052aba7d1088e81917d200dc7a4
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-09-08 08:51:30 +0200

    Update cursor.jsx

--- react/cursor/cursor.jsx
+++ react/cursor/cursor.jsx
@@ -1,1 +1,35 @@
 
+import { motion, useMotionValue, useSpring } from "framer-motion";
+import { useEffect } from "react";
+
+export default function Home() {
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = { damping: 120, stiffness: 1000 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e) => {
+      cursorX.set(e.clientX - 16);
+      cursorY.set(e.clientY - 16);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      className="cursor"
+      style={{
+        translateX: cursorXSpring,
+        translateY: cursorYSpring,
+      }}
+    />
+  );
+}
