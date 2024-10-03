0e215b4dec1e6d4a165caebb1ad018e8f6c4c827
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-10-03 19:48:59 +0200

    Update scroll.jsx

--- react/parallax-horizontal/scroll.jsx
+++ react/parallax-horizontal/scroll.jsx
@@ -1,1 +1,32 @@
 
+/*********************************************
+ * horizontal parallax section
+ *********************************************/
+import ParallaxText from "./ParallaxText";
+
+function ParallaxScroll() {
+  return (
+    <div className="parallax-wrap">
+      <ParallaxText baseVelocity={2}>
+        <img src="/images/logo.svg" alt="logo" />
+        lorem1
+      </ParallaxText>
+
+      <ParallaxText baseVelocity={-2}>
+         <img src="/images/logo.svg" alt="logo" />
+        lorem14
+      </ParallaxText>
+
+      <ParallaxText baseVelocity={2}>
+         <img src="/images/logo.svg" alt="logo" />
+        lorem1555
+      </ParallaxText>
+
+      <ParallaxText baseVelocity={-2}>
+       <img src="/images/logo.svg" alt="logo" />
+        lorem1222
+      </ParallaxText>
+    </div>
+  );
+}
+export default ParallaxScroll;
