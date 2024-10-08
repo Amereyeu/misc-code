e911c24555cc3070d97c17e6bf1786b0ef8a89e6
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-10-08 16:42:44 +0200

    Update dropdown.jsx

--- react/dropdown/dropdown.jsx
+++ react/dropdown/dropdown.jsx
@@ -1,1 +1,37 @@
 
+/*********************************************
+ * dropdown menu
+ *********************************************/
+import { Link } from "react-router-dom";
+
+function Dropdown() {
+  const items = ["one", "two", "three", "four"];
+
+  console.log(items);
+
+  return (
+    <div className="select-box-wrap">
+      <div className="select-box">
+        <div className="select-box__current" tabIndex="1">
+          <div className="select-box__current__value">
+            <Link to="#">ALL</Link>
+          </div>
+
+          <div className="select-box__current__icon"></div>
+        </div>
+
+        <ul className="select-box__list">
+          {items.map((category, i) => (
+            <li className="select-box__list__option" key={i}>
+              <Link className="select-box__list__option__item" to="#">
+                {category}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
