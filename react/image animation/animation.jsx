508007b2b621c8a7f0a8339300ad64c55a4a737a
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-11-26 10:45:18 +0100

    Update animation.jsx

--- react/image animation/animation.jsx
+++ react/image animation/animation.jsx
@@ -1,1 +1,46 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+import imageItems from "./gallery";
+
+function ImageAnimation() {
+  const [data] = useState(imageItems);
+
+  const rotation = [10, -20, -5, 5, -20];
+
+  const imageVariants = {
+    initial: () => ({
+      scale: 0,
+    }),
+    animate: (i) => ({
+      scale: 1,
+      rotate: rotation[i],
+    }),
+    transition: (i) => ({
+      delay: i * 0.1,
+      duration: 0.5,
+      type: "spring",
+      stiffness: 360,
+      damping: 20,
+    }),
+  };
+
+  return (
+    <motion.div className="image-animation-wrap">
+      {data.map((item, i) => (
+        <motion.img
+          src={item}
+          alt={i}
+          className="image"
+          key={i}
+          initial={imageVariants.initial()}
+          animate={imageVariants.animate(i)}
+          transition={imageVariants.transition(i)}
+        />
+      ))}
+    </motion.div>
+  );
+}
+
+export default ImageAnimation;
 
