075ba78c48104ee67e84ed4349648d7f3dbac85d
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-11-27 20:56:30 +0100

    Update data.js

--- react/image animation/data.js
+++ react/image animation/data.js
@@ -1,1 +1,15 @@
 
+/*********************************************
+ * images for animation
+ *********************************************/
+
+const imageItems = [
+  "images/engine.jpg",
+  "images/frame.jpg",
+  "images/sedan.jpg",
+  "images/merc.jpg",
+  "images/work.jpg",
+  "images/tools.jpg",
+];
+
+export default imageItems;
