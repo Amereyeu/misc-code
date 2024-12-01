71d3fc72e11dde8d6d9fc44d90714ea4ab577d17
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-12-01 19:01:36 +0100

    Update gridmulltiple.jsx

--- react/grid/gridmulltiple.jsx
+++ react/grid/gridmulltiple.jsx
@@ -1,1 +1,35 @@
+import { motion } from "framer-motion";
 
+function Tiles({ rows: r, cols: c }) {
+  const rows = new Array(r || 100).fill(1);
+  const cols = new Array(c || 10).fill(1);
+
+  const bg = {
+    backgroundColor: "#000",
+  };
+
+  return (
+    <div className="tiles-wrap">
+      {rows.map((_, i) => (
+        <motion.div key={`row` + i} className="tiles-row">
+          {cols.map((_, j) => (
+            <motion.div
+              className="tile"
+              whileHover={{
+                backgroundColor: `#39f`,
+                transition: { duration: 0 },
+              }}
+              animate={{
+                transition: { duration: 2 },
+              }}
+              key={`col` + j}
+              style={bg}
+            />
+          ))}
+        </motion.div>
+      ))}
+    </div>
+  );
+}
+
+export default Tiles;
