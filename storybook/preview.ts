3a38fedc3354e65549f3cbee6abfda305f9ad1f8
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-12-14 21:58:46 +0100

    Update preview.ts

--- storybook/preview.ts
+++ storybook/preview.ts
@@ -1,1 +1,45 @@
 
+import "../src/index.scss";
+
+import type { Preview } from "@storybook/react";
+
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+
+const kindleViewports = {
+  kindleFire2: {
+    name: "Kindle Fire 2",
+    styles: {
+      width: "600px",
+      height: "963px",
+    },
+  },
+  kindleFireHD: {
+    name: "Kindle Fire HD",
+    styles: {
+      width: "533px",
+      height: "801px",
+    },
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        ...kindleViewports,
+        ...INITIAL_VIEWPORTS,
+        defaultViewport: "Tablet",
+      },
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
