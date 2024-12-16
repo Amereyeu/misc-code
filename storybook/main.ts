5f88aa42c5a5734874d424e347e320aef2c4d7a2
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-12-16 19:23:36 +0100

    Update main.ts

--- storybook/main.ts
+++ storybook/main.ts
@@ -1,1 +1,29 @@
+import type { StorybookConfig } from "@storybook/react-vite";
 
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "storybook-addon-remix-react-router",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+  managerHead: () => `
+        <style>
+            .css-32o4gv {
+                height: 24px; 
+            }
+            .css-poqyku {
+                display: none;
+            }
+        </style>
+    `,
+};
+export default config;
