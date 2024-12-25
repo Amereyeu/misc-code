524dbb6adb91e5ef671edab77791dd1265b98c67
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-12-26 00:03:19 +0100

    Update words.jsx

--- react/text reveal/words.jsx
+++ react/text reveal/words.jsx
@@ -1,1 +1,39 @@
 
+import { useScroll } from "framer-motion";
+import { useRef } from "react";
+import Word from "./Word";
+
+function Words({ value }) {
+  const element = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start 0.9", "start 0.25"],
+  });
+
+  const words = value.split(" ");
+
+  return (
+    <p
+      className="text-reveal"
+      ref={element}
+    >
+      {words.map((word, index) => {
+        const start = index / words.length;
+        const end = start + 1 / words.length;
+
+        return (
+          <Word
+            key={index}
+            progress={scrollYProgress}
+            range={[start, end]}
+          >
+            {word}
+          </Word>
+        );
+      })}
+    </p>
+  );
+}
+
+export default Words;
