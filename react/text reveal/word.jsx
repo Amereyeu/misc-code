1a4dbf934812102676601678e36f0e7e367c45cc
Amerey <59344168+Amereyeu@users.noreply.github.com>
2024-12-27 20:44:43 +0100

    Update word.jsx

--- react/text reveal/word.jsx
+++ react/text reveal/word.jsx
@@ -1,1 +1,16 @@
 
+import { motion, useTransform } from "framer-motion";
+
+function Word({ children, range, progress }) {
+  const opacity = useTransform(progress, range, [0, 1]);
+  
+  return (
+    <span className="word">
+      <span className="word__span">{children}</span>
+
+      <motion.span style={{ opacity }}>{children}</motion.span>
+    </span>
+  );
+}
+
+export default Word;
