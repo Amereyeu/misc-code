ef6d6a9f11d1a18f30dee0d1417898c4770de371
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-02 16:56:10 +0100

    Update paragraph.jsx

--- react/text reveal/paragraph.jsx
+++ react/text reveal/paragraph.jsx
@@ -1,1 +1,24 @@
 
+import { motion, useScroll } from "framer-motion";
+import { useRef } from "react";
+
+function Paragraph({ value }) {
+  const element = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: element,
+    offset: ["start end", "start start"],
+  });
+
+  return (
+    <motion.p
+      className="text-reveal"
+      ref={element}
+      style={{ opacity: scrollYProgress }}
+    >
+      {value}
+    </motion.p>
+  );
+}
+
+export default Paragraph;
