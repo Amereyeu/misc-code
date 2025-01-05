4ed2c031c5236051371d83d1ff252b9166cf2ea2
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-05 12:11:42 +0100

    Update TextScroll.jsx

--- react/text reveal/TextScroll.jsx
+++ react/text reveal/TextScroll.jsx
@@ -1,1 +1,28 @@
+/*********************************************
+ * text reveal word by word with framer motion
+ * on page scroll
+ *********************************************/
 
+import Paragraph from "./Paragraph";
+import Words from "./Words";
+
+function TextScroll() {
+  const paragraph =
+    "This is a really long paragraph that I want to display on the screen, so I am going to write a lot of words to make sure that it is long enough to fill the screen.";
+
+  return (
+    <div className="text-reveal-wrap">
+      <div className="h-screen"></div>
+
+      <Words value={paragraph} />
+
+      <div className="h-screen"></div>
+
+      <Paragraph value={paragraph} />
+
+      <div className="h-screen"></div>
+    </div>
+  );
+}
+
+export default TextScroll;
