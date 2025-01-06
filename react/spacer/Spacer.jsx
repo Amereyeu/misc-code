8d6adaf2ae3ab0faf4a227a98cab02084e718fb9
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-06 17:38:47 +0100

    Update Spacer.jsx

--- react/spacer/Spacer.jsx
+++ react/spacer/Spacer.jsx
@@ -1,1 +1,18 @@
 
+function Spacer() {
+  return (
+    <div
+      className="spacer"
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <p>spacer</p>
+    </div>
+  );
+}
+
+export default Spacer;
