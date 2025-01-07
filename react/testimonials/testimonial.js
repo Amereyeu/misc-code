49c0a47cf4312e95c987fa543025b6e60001571b
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-07 16:14:46 +0100

    Update testimonial.js

--- react/testimonials/testimonial.js
+++ react/testimonials/testimonial.js
@@ -1,1 +1,57 @@
+/*********************************************
+ * data for testimonials
+ *********************************************/
+
+const testimonials = [
+  {
+    id: 1,
+    quote:
+      "Minima aperiam non, possimus voluptatibus architecto tenetur expedita dolores vero delectus autem velit. Ea eius in mollitia quis temporibus odit facere rem.",
+    name: "Minima ",
+    designation: "Product Manager ",
+    src: "images/engine.jpg",
+  },
+  {
+    id: 2,
+    quote:
+      "Sapiente temporibus error ea sit inventore, eveniet quis enim dicta ab odio earum deserunt illum cum soluta quae adipisci porro sequi rem.",
+    name: "Michael ",
+    designation: "CTO ",
+    src: "images/car.jpg",
+  },
+  {
+    id: 3,
+    quote:
+      "Sed, provident cumque, rem cupiditate sequi possimus id vel iusto quidem quam magnam voluptate quae dolorum rerum natus eligendi recusandae aliquam! Nihil!",
+    name: "Emily ",
+    designation: "Operations Director",
+    src: "images/merc.jpg",
+  },
+  {
+    id: 4,
+    quote:
+      "Minima aperiam non, possimus voluptatibus architecto tenetur expedita dolores vero delectus autem velit. Ea eius in mollitia quis temporibus odit facere rem.",
+    name: "Sarah ",
+    designation: "Product Manager ",
+    src: "images/engine.jpg",
+  },
+  {
+    id: 5,
+    quote:
+      "Sapiente temporibus error ea sit inventore, eveniet quis enim dicta ab odio earum deserunt illum cum soluta quae adipisci porro sequi rem.",
+    name: "Michael ",
+    designation: "CTO ",
+    src: "images/car.jpg",
+  },
+  {
+    id: 6,
+    quote:
+      "Sed, provident cumque, rem cupiditate sequi possimus id vel iusto quidem quam magnam voluptate quae dolorum rerum natus eligendi recusandae aliquam! Nihil!",
+    name: "Sed ",
+    designation: "Operations Director",
+    src: "images/merc.jpg",
+  },
+];
+
+export default testimonials;
 
