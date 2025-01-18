ae1d1f64919c5173ad4c95d1d4f8868955bb9d95
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-18 17:42:28 +0100

    Update SliderVertical.jsx

--- react/vertical scroll/SliderVertical.jsx
+++ react/vertical scroll/SliderVertical.jsx
@@ -1,1 +1,27 @@
 
+import { useState } from "react";
+
+import InfiniteSlider from "./InfiniteSlider";
+import galleryItems from "./gallery";
+
+function SliderVertical() {
+  const [data] = useState(galleryItems);
+
+  return (
+    <div className="infinite-slider-wrap">
+      <InfiniteSlider direction="vertical">
+        {data.map((item, index) => (
+          <img src={item} alt={index} className="box__image" key={index} />
+        ))}
+      </InfiniteSlider>
+
+      <InfiniteSlider direction="vertical" reverse>
+        {data.map((item, index) => (
+          <img src={item} alt={index} className="box__image" key={index} />
+        ))}
+      </InfiniteSlider>
+    </div>
+  );
+}
+
+export default SliderVertical;
