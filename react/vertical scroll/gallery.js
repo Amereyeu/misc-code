a0ce06155942dadca1cd756334dad0fa5be8a201
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-19 13:24:30 +0100

    Update gallery.js

--- react/vertical scroll/gallery.js
+++ react/vertical scroll/gallery.js
@@ -1,1 +1,15 @@
+const galleryItems = [
+  "images/engine.jpg",
+  "images/frame.jpg",
+  "images/sedan.jpg",
+  "images/merc.jpg",
+  "images/work.jpg",
+  "images/tools.jpg",
+  "images/sedan.jpg",
+  "images/merc.jpg",
+  "images/engine.jpg",
+  "images/frame.jpg",
+];
+
+export default galleryItems;
 
