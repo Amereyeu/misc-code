6563204fa14b35ecad4648991ef0af7a02dc2d9f
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-01-24 12:18:33 +0100

    Update Card.jsx

--- react/animated-background/Card.jsx
+++ react/animated-background/Card.jsx
@@ -1,1 +1,37 @@
 
+
+import { useState } from "react";
+
+import Background from "./Background";
+import cardItems from "./cards";
+
+function Card() {
+  const [data] = useState(cardItems);
+
+  return (
+    <div className="box-wrap">
+      <Background
+        className="bg"
+        transition={{
+          type: "spring",
+          bounce: 0.2,
+          duration: 0.6,
+        }}
+        enableHover
+      >
+        {data.map((item, index) => (
+          <div
+            key={index}
+            data-id={`card-${index + 1}`}
+          >
+            <div className="card">
+              <h3 className="card__header">{item.title}</h3>
+              <p className="card__text">{item.description}</p>
+            </div>
+          </div>
+        ))}
+      </Background>
+    </div>
+  );
+}
+export default Card;
