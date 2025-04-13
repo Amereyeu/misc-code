d922a7534d833c8473a78c25b142eed6fc789c09
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-04-13 07:51:42 +0200

    Update NoPage.jsx

--- react/404 page/NoPage.jsx
+++ react/404 page/NoPage.jsx
@@ -8,6 +8,7 @@ function NoPage() {
   return (
     <div className="notfound">
       <h1>Page not found</h1>
+      
       <Link to="/" className="notfound__link">
         Back to homepage
       </Link>
