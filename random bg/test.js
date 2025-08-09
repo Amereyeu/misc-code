21b8fc80f2c005a950c0fc3b074d8cf5ce2fae35
Amerey <59344168+Amereyeu@users.noreply.github.com>
2025-08-09 19:25:16 +0200

    Delete random bg/test.js

--- random bg/test.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("btn").addEventListener("click", function () {
-  var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
-
-  document.documentElement.style.setProperty("--random-color", randomColor);
-});
